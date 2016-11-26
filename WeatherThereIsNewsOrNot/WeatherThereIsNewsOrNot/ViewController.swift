6e169157d88faf15ccb920bbb64bb0b4f61bea64
Nathan Lambson <nathanlambson@Nathans-MacBook-Pro.local>
2016-11-25 23:10:57 -0700

    So it begins

---
+++ WeatherThereIsNewsOrNot/WeatherThereIsNewsOrNot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherThereIsNewsOrNot
+//
+//  Created by Nathan Lambson on 11/25/16.
+//  Copyright © 2016 Nathan Lambson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
